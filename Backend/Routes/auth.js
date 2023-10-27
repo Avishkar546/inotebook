7968ebce4dfa1a7f453174dd8f50ee53efc159d9
Avishkar <gaikwadavishkar546@gmail.com>
2023-10-27 13:23:27 +0530

    Login route is ready with JWT auhtentication

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -3,14 +3,26 @@ const User = require("../models/Users"); // Use this model to store data accordi
 const { body, validationResult } = require("express-validator"); // To add custom validation like minimum password length,or isEmail
 const router = express.Router(); // To respond to the router 
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-router.post("/",
-    // [
-    //     body('password',"Make a stronger password").isLength({min:5}),
-    //     body("Name","Name cannot be empty").isLength({min:1}), 
-    //     body("Email","Invalid user email").isEmail()
-    // ],
+const jwtAuthKey = "Gcoe@r@N@vod@y@"; // Secret key for JWT authentication.
+
+// Create a route to register a new user
+router.post("/registration",
+    [
+        body('password', "Make a stronger password").isLength({ min: 5 }),
+        body("Name", "Name cannot be empty").isLength({ min: 1 }),
+        body("Email", "Invalid user email").isEmail()
+    ],
     (req, res) => {
+        const errors = validationResult(req); // Catch the errors if any in the credentials.
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                errors: errors.array()
+            });
+        }
+
         const { Name, Email, password } = req.body; // Array destructuring to extract fiels from body.
 
         if (!Name || !Email || !password) { //No fields should be empty
@@ -28,7 +40,14 @@ router.post("/",
                         const user = new User({ Name, Email, password });
                         user.save()
                             .then(() => {
-                                res.status(201).json({ msg: "Registered Successfully" });
+                                let data = {
+                                    user: {
+                                        id: user.id
+                                    }
+                                }
+                                var jwtToken = jwt.sign(data, jwtAuthKey);
+                                console.log(jwtToken);
+                                res.status(201).json({ jwtToken});
                             })
                             .catch(err => {
                                 console.log(err);
@@ -41,4 +60,47 @@ router.post("/",
             .catch(error => console.log(error));
     })
 
-module.exports = router;
+// Login Authentication Route
+router.post("/login",
+    [
+        body('password', "Make a stronger password").exists(),
+        body("Email", "Invalid user email").isEmail()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req); // Catch the errors if any in the credentials.
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                errors: errors.array()
+            });
+        }
+
+        const { Email, password } = req.body;// As we are using ES6 Array destructuring please provide the same name as come in req.body
+
+        try {
+            let user = await User.findOne({Email});
+            if (!user) {
+                return res.status(400).json("Please provide the valid credentials");
+            }
+
+            let passwordCompare = await bcrypt.compare(password, user.password);
+
+            if (!passwordCompare) {
+                return res.status(400).json("Please provide the valid credentials");
+            }
+            let data = {
+                user: {
+                    id: user.id
+                }
+            }
+            var jwtToken = jwt.sign(data, jwtAuthKey);
+            // console.log(jwtToken);
+            res.status(201).json({ jwtToken});
+
+        } catch {
+            res.status(500).json("Internal Server error");
+        }
+
+    });
+
+module.exports = router; 
